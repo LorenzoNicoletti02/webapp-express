32af0abfbab659ba107bdabff13d2c1cd3db547f
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2025-01-23 17:51:39 +0100

    feat: index endpoint

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,7 +3,18 @@ const connection = require("../db");
 
 // Definiamo le callback per gli endpoint. Codice che viene  eseguito ad un URL specifico
 function index(req, res) {
-  res.json("Endpoint index");
+  const sql = "SELECT * FROM movies";
+
+  connection.query(sql, (err, movies) => {
+    if (err) {
+      return next(new Error(err.message));
+    }
+
+    return res.status(200).json({
+      status: "success",
+      data: movies,
+    });
+  });
 }
 
 function show(req, res) {
